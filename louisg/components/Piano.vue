d89820fb5665fdfd4169b951960151d29aa1ffc2
lgmb10 <louisgambier10@gmail.com>
2023-10-16 16:09:14 +0200

    first commit

---
+++ louisg/components/Piano.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>coucou</p>
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+export default {
+  name: "Piano"
+}
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+</script>
+
+<style scoped>
+
+</style>
