c147debb816328d1934ffb2d6ee92c85c3601486
Louis Gambier <louisgambier@MacBook-Pro-de-Louis.local>
2023-11-03 14:46:02 +0100

    Updates

--- assets/cv.88850e99.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./_plugin-vue_export-helper.c27b6911.js";import{c as _,o,p as s,e as t,a}from"./entry.f7c3cd4f.js";const p=e=>(s("data-v-1faf8c91"),e=e(),t(),e),d=p(()=>a("embed",{src:"/cv_louis_gambier.pdf"},null,-1)),r=[d],n={__name:"cv",setup(e){return(f,l)=>(o(),_("div",null,r))}},m=c(n,[["__scopeId","data-v-1faf8c91"]]);export{m as default};
---
+++ assets/empty.6222a2ad.js
@@ -0,0 +1,1 @@
+import{_ as r,q as t}from"./entry.7ee8c351.js";const s={};function n(e,o){return t(e.$slots,"default")}const _=r(s,[["render",n]]);export{_ as default};
